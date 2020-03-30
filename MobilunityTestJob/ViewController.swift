0bed7f1a35d5e51a0a85fe259e7a2b58c41fd486
ZhurbiloYaroslav <zhurbilo.yaroslav@gmail.com>
2020-03-30 18:14:41 +0300

    Initial Commit

---
+++ MobilunityTestJob/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobilunityTestJob
+//
+//  Created by Yaroslav Zhurbilo on 30.03.20.
+//  Copyright © 2020 Yaroslav Zhurbilo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
