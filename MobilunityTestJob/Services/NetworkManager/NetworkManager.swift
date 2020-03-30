56ee6cb14bab0d551d4a2c06f7a020e599262207
ZhurbiloYaroslav <zhurbilo.yaroslav@gmail.com>
2020-03-30 20:19:33 +0300

    NEW Added folder with 'NetworkManager'

---
+++ MobilunityTestJob/Services/NetworkManager/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  MobilunityTestJob
+//
+//  Created by Yaroslav Zhurbilo on 30.03.20.
+//  Copyright © 2020 Yaroslav Zhurbilo. All rights reserved.
+//
+
+import Foundation
