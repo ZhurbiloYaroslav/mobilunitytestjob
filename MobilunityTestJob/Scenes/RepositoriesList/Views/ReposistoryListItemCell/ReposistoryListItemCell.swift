93a98de0acac166ccfe8ccf492a01dcd6174f7b5
ZhurbiloYaroslav <zhurbilo.yaroslav@gmail.com>
2020-03-31 22:31:44 +0300

    NEW Made dynamic height for RepositoryListItemCell;

--- MobilunityTestJob/Scenes/RepositoriesList/Views/ReposistoryListItemCell/ReposistoryListItemCell.swift
+++ MobilunityTestJob/Scenes/RepositoriesList/Views/ReposistoryListItemCell/ReposistoryListItemCell.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import BonMot
 
 class ReposistoryListItemCell: UICollectionViewCell {
+    typealias `Self` = ReposistoryListItemCell
 
     @IBOutlet weak var repositoryName: UILabel!
     @IBOutlet weak var repositoryDetails: UILabel!
@@ -20,13 +22,41 @@ class ReposistoryListItemCell: UICollectionViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        repositoryName.text = ""
-        repositoryDetails.text = ""
+        repositoryName.attributedText = nil
+        repositoryDetails.attributedText = nil
     }
 
     func configure(with model: RepositoriesListViewModelProtocol) {
-        repositoryName.text = model.repositoryName
-        repositoryDetails.text = model.repositoryDetails
+        repositoryName.attributedText = model.repositoryName.styled(with: Self.titleStyle)
+        repositoryDetails.numberOfLines = 3
+        repositoryDetails.lineBreakMode = .byWordWrapping
+        repositoryDetails.attributedText = model.repositoryDetails.styled(with: Self.subtitleStyle)
+    }
+    
+    static func height(for width: CGFloat, title: String?, subtitle: String?) -> CGFloat {
+        let availableWidth = width - edges.left - edges.right
+        let size = CGSize(width: availableWidth, height: .greatestFiniteMagnitude)
+        let options: NSStringDrawingOptions = [.usesFontLeading, .usesLineFragmentOrigin]
+        let titleHeight = (title ?? "").styled(with: titleStyle).boundingRect(with: size, options: options, context: nil).height
+        let subtitleHeight = (subtitle ?? "").styled(with: subtitleStyle).boundingRect(with: size, options: options, context: nil).height
+        return max(minHeight, edges.top + ceil(titleHeight + subtitleHeight) + edges.bottom)
     }
 
+}
+
+// MARK: - Cell styles
+extension ReposistoryListItemCell {
+    private static let minHeight: CGFloat = 20
+    private static let edges: UIEdgeInsets = {
+        let middleAndBottom: CGFloat = 8 + 8
+        return UIEdgeInsets(top: 8, left: 16, bottom: middleAndBottom, right: 16)
+    }()
+    private static let titleStyle: StringStyle = {
+        StringStyle(.font(UIFont.defaultFont(.regular, size: 16)),
+                    .color(.white))
+    }()
+    private static let subtitleStyle: StringStyle = {
+        StringStyle(.font(UIFont.defaultFont(.regular, size: 14)),
+                    .color(.white))
+    }()
 }
